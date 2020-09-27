2e5b578feea3d503bfc99f1fb9fe05ab6dcae8ff
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-09-27 07:36:36 -0400

    List of owners

--- src/main/java/sfgpetclinic/demo/controllers/OwnerController.java
+++ src/main/java/sfgpetclinic/demo/controllers/OwnerController.java
@@ -1,13 +1,25 @@
 package sfgpetclinic.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import sfgpetclinic.demo.services.OwnerService;
+
 @RequestMapping("/owners")
 //localgost:8080/owners/
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+
     }
 }
