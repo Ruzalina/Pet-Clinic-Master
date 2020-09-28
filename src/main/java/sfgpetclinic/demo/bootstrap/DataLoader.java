c8515448341d5a20e32541621d802baca2dacc92
ruzalina <sulejmanova.ruzalina@bk.ru>
2020-09-28 07:43:48 -0400

    Auto GenerateMap IDs

--- src/main/java/sfgpetclinic/demo/bootstrap/DataLoader.java
+++ src/main/java/sfgpetclinic/demo/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
         @Override
         public void run (String...args) throws Exception {
             Owner owner1=new Owner();
-            owner1.setId(1L);
             owner1.setFirsName("Michael");
             owner1.setLastName("Weston");
 
             ownerService.save(owner1);
 
             Owner owner2=new Owner();
-            owner2.setId(2L);
             owner2.setFirsName("Fiona");
             owner2.setLastName("Glenanne");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
             System.out.println("Loaded Owners...");
 
             Vet vet1=new Vet();
-            vet1.setId(1L);
             vet1.setFirsName("Sam");
             vet1.setLastName("Axe");
 
             vetService.save(vet1);
 
             Vet vet2=new Vet();
-            vet2.setId(2L);
             vet2.setFirsName("Jessie");
             vet2.setLastName("Porter");
 
--- src/main/java/sfgpetclinic/demo/services/map/PetServiceMap.java
+++ src/main/java/sfgpetclinic/demo/services/map/PetServiceMap.java
@@ -19,7 +19,7 @@ public class PetServiceMap extends AbstarctMapService<Pet,Long>implements PetSer
 
     @Override
     public Pet save(Pet object) {
-        return super.save(object.getId(),object);
+        return super.save(object);
     }
 
     @Override
